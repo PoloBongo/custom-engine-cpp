2f0baef0689fd390f14197742208d150de31e498
DemonXlegenD <flahalle@gaming.tech>
2024-03-19 13:56:37 +0100

    Ajout Librairies

---
+++ Engine/compile.bat
@@ -0,0 +1,3 @@
+C:\Users\33673\gtech_2eme_annee\Engine\Librairies\VulkanSDK\Bin\glslc.exe Shaders\simple_shader.vert -o Shaders\simple_shader.vert.spv
+C:\Users\33673\gtech_2eme_annee\Engine\Librairies\VulkanSDK\Bin\glslc.exe Shaders\simple_shader.frag -o Shaders\simple_shader.frag.spv
+pause
